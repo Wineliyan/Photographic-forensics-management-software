b4e38ba24f7a8120d24c77a5a1d6c6adf7b1a55a
Wineliyan <145823469+Wineliyan@users.noreply.github.com>
2024-02-05 12:08:49 +0800

    Add files via upload

---
+++ src/main/java/com/ly/Photographicforensicsmanagementsoftware/PhotographicForensicsManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.ly.Photographicforensicsmanagementsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotographicForensicsManagementSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotographicForensicsManagementSoftwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ly/Photographicforensicsmanagementsoftware/PhotographicForensicsManagementSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.Photographicforensicsmanagementsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotographicForensicsManagementSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
